0e7ff29b230b3b6a583ffef346661ac0ca01590d
RAllured <rallured@gmail.com>
2016-11-30 15:46:27 -0500

    Moved readCyl4D into cylinder.py.

--- imaging/analysis.py
+++ imaging/analysis.py
@@ -164,38 +164,3 @@ def getSubApp(img,log=False,points=None):
     else:
         x,y = points
     return img[y.min():y.max(),x.min():x.max()]
-
-def readCyl4D(fn,rotate=np.linspace(-.75,-1.25),interp=None):
-    """
-    Load in data from 4D measurement of cylindrical mirror.
-    Scale to microns, remove misalignments,
-    strip NaNs.
-    If rotate is set to an array of angles, the rotation angle
-    which minimizes the number of NaNs in the image after
-    stripping perimeter nans is selected.
-    Distortion is bump positive looking at concave surface.
-    Imshow will present distortion in proper orientation as if
-    viewing the concave surface.
-    """
-    #Remove NaNs and rescale
-    d = np.genfromtxt(fn,skip_header=12,delimiter=',')
-    d = man.stripnans(d)
-    d = d *.6328
-    d = d - np.nanmean(d)
-
-    #Remove cylindrical misalignment terms
-    d = d - fit.fitCylMisalign(d)[0]
-
-    #Interpolate over NaNs
-    if interp is not None:
-        d = man.nearestNaN(d,method=interp)
-
-    #Rotate out CGH roll misalignment?
-    if rotate is not None:
-        b = [np.sum(np.isnan(\
-            man.stripnans(\
-                nd.rotate(d,a,order=1,cval=np.nan)))) for a in rotate]
-        return man.stripnans(\
-            nd.rotate(d,rotate[np.argmin(b)],order=1,cval=np.nan))
-
-    return d
