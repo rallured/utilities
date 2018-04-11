70aa08a2cf17e487dac4bd0b5c466ef0eac22b5b
Casey DeRoo <Casey@Caseys-MacBook-Pro.local>
2018-04-11 14:46:42 -0400

    ReadConicAdditions

--- metrology.py
+++ metrology.py
@@ -84,6 +84,7 @@ def readCyl4D(fn,rotate=np.linspace(.75,1.5,50),interp=None):
     d = d *.6328
     d = d - np.nanmean(d)
 
+    d = np.rot90(d,k = 2)
     #Remove cylindrical misalignment terms
     d = d - fit.fitCylMisalign(d)[0]
 
@@ -100,6 +101,47 @@ def readCyl4D(fn,rotate=np.linspace(.75,1.5,50),interp=None):
         d = man.nearestNaN(d,method=interp)
 
     return d,dx
+
+def readConic4D(fn,rotate=None,interp=None):
+    """
+    Load in data from 4D measurement of cylindrical mirror.
+    Scale to microns, remove misalignments,
+    strip NaNs.
+    If rotate is set to an array of angles, the rotation angle
+    which minimizes the number of NaNs in the image after
+    stripping perimeter nans is selected.
+    Distortion is bump positive looking at concave surface.
+    Imshow will present distortion in proper orientation as if
+    viewing the concave surface.
+    """
+    #Get xpix value in mm
+    l = getline(fn,9)
+    dx = float(l.split()[1])*1000.
+    
+    #Remove NaNs and rescale
+    d = np.genfromtxt(fn,skip_header=12,delimiter=',')
+    d = man.stripnans(d)
+    d = d *.6328
+    d = d - np.nanmean(d)
+    
+    d = np.rot90(d,k = 2)
+    #Remove cylindrical misalignment terms
+    conic_fit = fit.fitConic(d)
+    d = d - conic_fit[0]
+
+    #Rotate out CGH roll misalignment?
+    if rotate is not None:
+        b = [np.sum(np.isnan(\
+            man.stripnans(\
+                nd.rotate(d,a,order=1,cval=np.nan)))) for a in rotate]
+        d = man.stripnans(\
+            nd.rotate(d,rotate[np.argmin(b)],order=1,cval=np.nan))
+
+    #Interpolate over NaNs
+    if interp is not None:
+        d = man.nearestNaN(d,method=interp)
+
+    return d,dx,conic_fit[1]
 
 def readFlatScript(fn,interp=None):
     """
